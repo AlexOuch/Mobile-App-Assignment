db04207022a5b485df6d88b0dc7afec3612881ea
AlexOuch <6285@ait.nsw.edu.au>
2020-05-30 01:13:27 +1000

    first/second page

--- app/src/main/java/com/example/gamebase/MainActivity.java
+++ app/src/main/java/com/example/gamebase/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.gamebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton psScreenButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        psScreenButton = findViewById(R.id.psButton);
+        psScreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent psScreenIntent = new Intent(view.getContext(), SecondScreen.class);
+                startActivity(psScreenIntent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/gamebase/SecondScreen.java
@@ -0,0 +1,32 @@
+package com.example.gamebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondScreen extends AppCompatActivity {
+
+    Button buttonTest;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_screen);
+
+        buttonTest = findViewById(R.id.buttonTest);
+
+        buttonTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goBack = new Intent();
+
+                setResult(RESULT_OK, goBack);
+
+                //kill activity
+                finish();
+            }
+        });
+    }
+}

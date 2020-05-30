2483a0805df02071f1ab382e7abc4dc600c7dbcf
AlexOuch <6285@ait.nsw.edu.au>
2020-05-30 20:54:45 +1000

    added game page

--- app/src/main/java/com/example/gamebase/MainActivity.java
+++ app/src/main/java/com/example/gamebase/MainActivity.java
@@ -10,21 +10,76 @@ import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton searchButton;
+    ImageButton steamScreenButton;
+    ImageButton nintendoScreenButton;
     ImageButton psScreenButton;
+    ImageButton xboxScreenButton;
+    ImageButton playScreenButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        searchButton = findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        steamScreenButton = findViewById(R.id.steamButton);
+        steamScreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent secondScreenIntent = new Intent(view.getContext(), SecondScreen.class);
+                secondScreenIntent.putExtra("CONSOLE", "steam");
+                startActivity(secondScreenIntent);
+            }
+        });
+
+        nintendoScreenButton = findViewById(R.id.nintendoButton);
+        nintendoScreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent secondScreenIntent = new Intent(view.getContext(), SecondScreen.class);
+                secondScreenIntent.putExtra("CONSOLE", "nintendo");
+                startActivity(secondScreenIntent);
+            }
+        });
+
         psScreenButton = findViewById(R.id.psButton);
         psScreenButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent secondScreenIntent = new Intent(view.getContext(), SecondScreen.class);
-                secondScreenIntent.putExtra("CONSOLE", 3);
+                secondScreenIntent.putExtra("CONSOLE", "ps");
                 startActivity(secondScreenIntent);
             }
         });
+
+        xboxScreenButton = findViewById(R.id.xboxButton);
+        xboxScreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent secondScreenIntent = new Intent(view.getContext(), SecondScreen.class);
+                secondScreenIntent.putExtra("CONSOLE", "xbox");
+                startActivity(secondScreenIntent);
+            }
+        });
+
+        playScreenButton = findViewById(R.id.playButton);
+        playScreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent secondScreenIntent = new Intent(view.getContext(), SecondScreen.class);
+                secondScreenIntent.putExtra("CONSOLE", "play");
+                startActivity(secondScreenIntent);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/gamebase/SecondScreen.java
+++ app/src/main/java/com/example/gamebase/SecondScreen.java
@@ -6,19 +6,44 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SecondScreen extends AppCompatActivity {
 
     Button buttonTest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_screen);
 
         Intent activityThatCalled = getIntent();
-        int console = activityThatCalled.getExtras().getInt("CONSOLE");
+        String console = activityThatCalled.getExtras().getString("CONSOLE");
         buttonTest = findViewById(R.id.buttonTest);
 
+        TextView title;
+        title = findViewById(R.id.titleText);
+
+        if(console.equals("steam")){
+            title.setText("Steam");
+        }
+
+        if(console.equals("nintendo")){
+            title.setText("Nintendo");
+        }
+
+        if(console.equals("ps")){
+            title.setText("PlayStation");
+        }
+
+        if(console.equals("xbox")){
+            title.setText("XBox");
+        }
+
+        if(console.equals("play")){
+            title.setText("Google PlayStore");
+        }
         buttonTest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
